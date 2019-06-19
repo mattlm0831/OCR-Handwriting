8b2ea4addfe62544cf755ec16fbd7870917006dc
Matthew Mulhall <32942111+mattlm0831@users.noreply.github.com>
2019-06-19 16:22:33 -0400

    Update data-plotter.py

--- bin/src/testing/data-plotter.py
+++ bin/src/testing/data-plotter.py
@@ -11,18 +11,20 @@ import matplotlib.pyplot as plt
 
 HIST_FILE = r'C:\Users\mlm14013work\Desktop\OCR-Handwriting\bin\src\testing\convnet-smallset-ocr-test2\history.npy'
 
-history = np.load(HIST_FILE)
-history = history[()]
+def plot(file = HIST_FILE)
+  
+  history = np.load(file)
+  history = history[()]
 
-acc = history.history['acc']
-val_acc= history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+  acc = history.history['acc']
+  val_acc= history.history['val_acc']
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
 
-epochs = range(1, len(acc) + 1)
+  epochs = range(1, len(acc) + 1)
 
-plt.plot(epochs, acc, 'bo', label='Training Accuracy')
-plt.plot(epochs, val_acc, 'b', label='Validation Accuracy')
-plt.title('Training and Validation Accuracy')
-plt.legend()
-plt.figure()
+  plt.plot(epochs, acc, 'bo', label='Training Accuracy')
+  plt.plot(epochs, val_acc, 'b', label='Validation Accuracy')
+  plt.title('Training and Validation Accuracy')
+  plt.legend()
+  plt.figure()
