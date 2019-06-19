0626f9654b4de1c232088175796fa5f3269ba75c
Matthew Mulhall <32942111+mattlm0831@users.noreply.github.com>
2019-06-19 14:52:41 -0400

    Completed test 2, starting overall test 3

---
+++ bin/src/testing/data-plotter.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 13:56:27 2019
+
+@author: Matthew Mulhall
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+HIST_FILE = r'C:\Users\mlm14013work\Desktop\OCR-Handwriting\bin\src\testing\convnet-smallset-ocr-test2\history.npy'
+
+history = np.load(HIST_FILE)
+history = history[()]
+
+acc = history.history['acc']
+val_acc= history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.legend()
+plt.figure()
