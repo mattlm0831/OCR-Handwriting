8db95f0bc4cc4aeee09e6147bbef925d90881526
Matthew Mulhall <32942111+mattlm0831@users.noreply.github.com>
2019-06-25 16:43:00 -0400

    Many, many changes

--- bin/src/testing/data-plotter.py
+++ bin/src/testing/data-plotter.py
@@ -17,14 +17,23 @@ from keras.utils.vis_utils import model_to_dot
 HIST_FILE = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-medset-ocr-test1'
 new_hist = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-smallset-ocr-test2'
 medset_2 = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-medset-ocr-test2'
+medset_3 = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-medset-ocr-test3'
 
-mdl_path = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-smallset-ocr-test1\convnet-smallset-ocr-test1-model.h5'
+mdl_path1 = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-smallset-ocr-test1\convnet-smallset-ocr-test1-model.h5'
+mdl_path4 = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-medset-ocr-test2\convnet-medset-ocr-test2-model.h5'
+mdl_path5 = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-medset-ocr-test\convnet-medset-ocr-test3-model.h5'
 
-def plot(file):
+
+def plot(file = None, obj = None):
     
-     history = np.load(os.path.join(file, 'history.npy'))
-     history = history[()]
-    
+     if not obj and not file:
+         return "Cannot operate on nothing"
+     
+     if not obj:
+         history = np.load(os.path.join(file, 'history.npy'))
+         history = history[()]
+     if not file:
+         history = obj
      acc = history.history['acc']
      val_acc= history.history['val_acc']
      loss = history.history['loss']
@@ -57,8 +66,8 @@ def model_to_img(model_path):
      model = models.load_model(model_path)
      file_path = os.path.dirname(os.path.abspath(model_path))
      file_path = os.path.join(file_path, 'model.png')
-     plot_model(model, to_file = 'model.png', show_shapes = True, show_layer_names=True)
-     return
+     plot_model(model, to_file = file_path, show_shapes = True, show_layer_names=True)
+     return str("Saved to " + file_path)
 
 def mdl_dot(model):
-     SVG(model_to_dot(model).create(prog='dot', dormat = 'svg'))
+     SVG(model_to_dot(model).create(prog='dot', format = 'svg'))
