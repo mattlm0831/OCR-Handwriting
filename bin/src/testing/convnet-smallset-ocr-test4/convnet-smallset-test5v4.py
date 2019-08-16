a02b9982c9db7fa3c193dec07d808762da4e1b74
Matthew Mulhall <32942111+mattlm0831@users.noreply.github.com>
2019-08-16 14:37:55 -0400

    Many tests

--- bin/src/testing/convnet-smallset-ocr-test4/convnet-smallset-test5v4.py
+++ bin/src/testing/convnet-smallset-ocr-test4/convnet-smallset-test5v4.py
@@ -9,43 +9,48 @@ VALIDATION_DIR = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testi
 TEST_DIR = r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-smallset-ocr-test4\testing-data2\test\images'
 
 from keras import layers
-from keras import models 
+from keras import models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
-
+import AutoAiLib as ai
 
 def train():
     model = models.Sequential()
     
     model.add(layers.Conv2D(32, (3,3), activation='relu',
-                            input_shape=(150,150,3)))
+                            input_shape=(200,200,3)))
     model.add(layers.MaxPooling2D((2, 2)))
     
     
-    model.add(layers.Conv2D(64, (5, 5), activation = 'relu'))
+        
+    model.add(layers.Conv2D(64, (5,5), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(rate = .3))
+    
+    
+    model.add(layers.Conv2D(64, (3, 3), activation = 'relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Dropout(rate = .3))
     
     model.add(layers.Conv2D(128, (1, 1), activation = 'relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Dropout(rate=.3))
-           
-
-    model.add(layers.Conv2D(256, (3, 3), activation = 'relu'))
+    
+    model.add(layers.Conv2D(128, (3, 3), activation = 'relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Dropout(rate=.3))
        
+    
     model.add(layers.Conv2D(256, (3, 3), activation = 'relu'))
     model.add(layers.MaxPooling2D((2, 2)))
-       
+    
     model.add(layers.Flatten())
-    #new
-    model.add(layers.Dense(1024, activation='relu'))
-    #new
+    
     model.add(layers.Dense(512, activation='relu'))
     
     model.add(layers.Dense(4, activation='softmax'))
+    
     
     
     
@@ -60,29 +65,31 @@ def train():
                                        width_shift_range=0.2,
                                        height_shift_range=0.2,
                                        shear_range=0.2,
-                                       zoom_range=0.2,
-                                       horizontal_flip = True)
+                                       zoom_range=0.2)
     
     test_datagen = ImageDataGenerator(rescale=1./255)
     
     train_generator= train_datagen.flow_from_directory(
             TRAIN_DIR,
-            target_size=(150,150),
+            target_size=(200,200),
             batch_size=64,
             class_mode= 'categorical')
     
     validation_generator =  test_datagen.flow_from_directory(
             VALIDATION_DIR,
-            target_size=(150,150),
-            batch_size=64,
+            target_size=(200,200),
+            batch_size=64, 
             class_mode= 'categorical')
     
     history = model.fit_generator(train_generator,
                                   steps_per_epoch= 30,
-                                  epochs=250,
+                                  epochs=500,
                                   validation_data= validation_generator,
                                   validation_steps=50)
     
-    model.save("convnet-smallset-test3-model-TEST5v5.h5")
-    np.save("historytest5v5.npy", history)
+    tester = ai.convnet_tester(model, ['a','b','c','d'])
+    tester.predict_many(r'C:\Users\matth\Documents\GitHub\OCR-Handwriting\bin\src\testing\convnet-smallset-ocr-test4\testing-data3\test')
+    
+    model.save("convnet-smallset-test3-model-TEST5v8.h5")
+    np.save("historytest5v8.npy", history)
     
